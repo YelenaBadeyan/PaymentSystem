e24397017875d7cc3a7066b7f47da8e54af285ec
Yelena Badeyan <yelena.badeyan@idt.am>
2024-10-18 23:29:59 +0400

    Adding RequestLoggingMiddleware

--- PaymentSystem/Program.cs
+++ PaymentSystem/Program.cs
@@ -11,24 +11,23 @@ builder.Services.AddDbContext<AppDBContext>(options =>
     options.UseSqlServer(connectionString);
 });
 
-// Add services to the container.
 
-//TODO Understand this line more deeply
 builder.Services.AddTransient<UsersService>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<RequestLoggingMiddleware>();
 
 app.UseHttpsRedirection();
 
